7682164227660695f0e84c835d86c433486bf49e
Mantas Baltakis <mantas.baltakis@gmail.com>
2018-04-27 13:22:49 +0100

    Kata-hira switch

--- js/index.js
+++ js/index.js
@@ -8,3 +8,24 @@ function btnAnimation() {
         $(this).animate({opacity: '0.4'}, "fast");
         $(this).animate({opacity: '1'}, "fast");
     }
+
+
+// Nav bar, for switching between Hiragana and Katakana
+document.getElementById("hiragana-switch").addEventListener("click",hiraganaSwitch, false);
+document.getElementById("katakana-switch").addEventListener("click",katakanaSwitch, false);
+
+
+function hiraganaSwitch() {
+    if ($("#katakana-content").css("display") != "none") {
+        $("#katakana-content").css("display", "none");
+        $("#hiragana-content").css("display", "block");
+    }
+};
+
+function katakanaSwitch() {
+    if ($("#hiragana-content").css("display") != "none") {
+        $("#hiragana-content").css("display", "none");
+        $("#katakana-content").css("display", "block");
+    }
+};
+
